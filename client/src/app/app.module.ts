ca6a0247baf8121acbcc9bda2d7f418b23ff3e2a
Roxanne Garcia <rxrgeneration@gmail.com>
2018-08-09 17:35:30 -0500

    Added page components and router. Started responsive styling.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { NavComponent } from './nav/nav.component';
+
+//  ==== ROUTER =======
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'tickets', component: TicketsComponent},
+  { path: 'form', component: UserFormComponent},
+  { path: 'checkout', component: CheckoutComponent},
+
+  //  home default path
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TicketsComponent,
+    UserFormComponent,
+    CheckoutComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
